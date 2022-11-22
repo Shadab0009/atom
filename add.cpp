1f91cd43f53271fb689e34ca236307671aeae4ba
Shadab <shadabansari201811@gmail.com>
2022-11-23 00:34:00 +0530

    Initial commit

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace sdt;
+int main(){
+  int a,b,c;
+  cout<"Enter two numbers"<<endl;
+  cin>>a>>b;
+  c = a+b;
+  cout<<"Sum of "<<a<<" and "<<b<<" is : "<<c<<endl;
+  // cout<<"Hello Atom"<<endl;
+  return 0;
+}
