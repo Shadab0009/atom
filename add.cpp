1d0e75fd622ce96c88c36a00825f3c23bf1f3611
Shadab <109566422+Shadab0009@users.noreply.github.com>
2022-11-27 20:25:07 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
-using namespace sdt;
+using namespace std;
 int main(){
   int a,b,c;
   cout<"Enter two numbers"<<endl;
   cin>>a>>b;
   c = a+b;
   cout<<"Sum of "<<a<<" and "<<b<<" is : "<<c<<endl;
-  // cout<<"Hello Atom"<<endl;
+  cout<<"Hello Atom"<<endl;
   return 0;
 }
